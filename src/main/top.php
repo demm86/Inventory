a2b373e1342fc038d6e9cff3242328399c76404c
davimon <davidmon@iadb.org>
2021-10-09 21:18:35 -0400

    change || title

--- src/main/top.php
+++ src/main/top.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Tables</title>
+    <title>Inventory</title>
 
     <!-- Custom fonts for this template -->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
